a6139748ac15c9bfc09acdcee8e3db4d27dcb2b7
tillias <ternovich@gmail.com>
2020-09-14 16:21:07 +0200

    Initial commit

---
+++ src/main/java/com/example/asyncws/AsyncwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.asyncws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncwsApplication.class, args);
+    }
+
+}
